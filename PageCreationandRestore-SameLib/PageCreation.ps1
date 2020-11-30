3fa665442fc9f2db028982e31cc1b640a7354026
Manoj Mittal <manoj.mcans@gmail.com>
2020-11-30 13:46:36 +0530

    Initial Check In
    Initial Check In

---
+++ PageCreationandRestore-SameLib/PageCreation.ps1
@@ -0,0 +1,23 @@
+﻿try 
+   {
+    $srcUrl = "Site Url" 
+    $pageName = "Home"   
+    
+     Connect-PnPOnline -Url $srcUrl
+     $tempFile = 'C:\MediaFiles\Test\'+ $pageName +'.xml'
+     Export-PnPClientSidePage -Force -Identity $pageName -Out $tempFile
+
+     $con = Get-Content $tempFile
+     $sourcepage=$pageName +".aspx"
+     $targetpage="Duplicate"+$pageName +".aspx"
+     
+     $con | % { $_.Replace($sourcepage, $targetpage) } | Set-Content $tempFile
+     Apply-PnPProvisioningTemplate -Path  $tempFile
+     write -host' Page created successfully'   
+   
+} 
+catch {
+    Write-Host - ForegroundColor Red 'Error ', ':'
+    $Error[0].ToString();
+   
+}
---
+++ PageCreationandRestore-SameLib/RevertPageChanges.ps1
@@ -0,0 +1,24 @@
+﻿try 
+   {
+    $srcUrl = "Site Url" 
+    
+    $pageName = "DuplicateHome"   
+    
+     Connect-PnPOnline -Url $srcUrl
+     $tempFile = 'C:\MediaFiles\Test\'+ $pageName +'.xml'
+     Export-PnPClientSidePage -Force -Identity $pageName -Out $tempFile
+
+     $con = Get-Content $tempFile
+     $sourcepage=$pageName +".aspx"
+     $targetpage="Home" +".aspx"
+     
+     $con | % { $_.Replace($sourcepage,$targetpage) } | Set-Content $tempFile
+     Apply-PnPProvisioningTemplate -Path  $tempFile
+     write -host' Page created successfully'   
+   
+} 
+catch {
+    Write-Host - ForegroundColor Red 'Error ', ':'
+    $Error[0].ToString();
+   
+}
